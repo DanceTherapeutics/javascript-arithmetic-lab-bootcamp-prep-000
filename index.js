0de2d46fdb99f368a1d3e05847b237cd712373de
DanceTherapeutics <dancetherapeutics@gmail.com>
2017-10-20 21:22:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ function makeInt(n){
   return parseInt(n, 10)
 }
 function preserveDecimal(n){
-  var n = ('2.222')
-  return parseFloat(n)
+  var n = parseFloat('2.222')
+  return parseInt(n)
 }
 
 function preserveDecimal(n){
