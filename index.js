331e2e565dfb27f0256c89791979361c63792d0d
DanceTherapeutics <dancetherapeutics@gmail.com>
2017-10-20 18:50:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,26 +2,34 @@
 function add(a,b){
   return a + b
 }
+
 function subtract(a,b){
   return a - b
 }
+
 function multiply(a,b){
   return a*b
 }
+
 function divide(a,b){
   return a/b
 }
+
 function inc(a){
   return a += 1
 }
+
 function dec(a){
   return a -= 1
 }
+
 function makeInt(a){
   var a = parseInt(a)
   return parseInt(a)
 }
+
 function makeInt(a){
   var a = parseInt('0x2328');
   return parseInt(a)
 }
+
